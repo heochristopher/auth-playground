b69c2c52e68c3db6f339f7b5d306851c7a6f5029
heochristopher <christopher.heo23@sitechhs.com>
2022-03-07 12:23:40 -0500

    asdf

--- dist/middleware/userMiddleware.js
+++ dist/middleware/userMiddleware.js
@@ -12,7 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.login = exports.createUser = exports.getUsers = void 0;
+exports.deleteUser = exports.updateUsers = exports.login = exports.createUser = exports.getUsers = void 0;
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const User_1 = require("../models/User");
 const bcryptjs_1 = __importDefault(require("bcryptjs"));
